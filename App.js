0671214d3b24d5ce9cf2b392749b42b222c35e45
Christopher Pedersen <chris@topherpedersen.com>
2022-07-26 13:31:08 -0500

    programmatically set position of cloned element

--- App.js
+++ App.js
@@ -8,18 +8,18 @@ import {
 } from 'react-native';
 
 const RedSquare = (props) => {
-  const positionStyle = props.isClone ? { top: 393.33334, left: 170 } : undefined;
-
-  // TODO: Get position of original element programmatically
+  const positionStyle = props.isClone ? { top: props.originalElementY, left: props.originalElementX } : undefined;
+  const neonGreen = "#39FF14";
 
   return (
     <View 
       ref={this.elementToBeClonedRef} 
-      style={[{ width: 50, height: 50, backgroundColor: 'red' }, positionStyle]}
+      style={[{ width: 50, height: 50, backgroundColor: neonGreen }, positionStyle]}
       onLayout={(event) => {
         if (!props.isClone) {
           const { x, y, width, height } = event.nativeEvent.layout;
-          // alert(`x: ${x} y: ${y}`);
+          props.setOriginalElementX(x);
+          props.setOriginalElementY(y);
         }
       }}
     >
@@ -31,12 +31,20 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.elementToBeClonedRef = React.createRef();
-
     this.state = {
       showModal: false,
+      originalElementX: 0,
+      originalElementY: 0,
     };
   }
+
+  setOriginalElementX = (x) => {
+    this.setState({ originalElementX: x});
+  };
+
+  setOriginalElementY = (y) => {
+    this.setState({ originalElementY: y});
+  };
 
   showTooltip = () => {
     this.setState({ showModal: true });
@@ -46,22 +54,20 @@ class App extends React.Component {
     return(
       <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>PrototypeTooltip</Text>
-        <RedSquare />
+        <RedSquare setOriginalElementX={this.setOriginalElementX} setOriginalElementY={this.setOriginalElementY} />
         <Button title="Show Tooltip" onPress={this.showTooltip} />
         <Modal
-          // animationType="slide"
           transparent={true}
-          // transparent={false}
           visible={this.state.showModal}
-          onRequestClose={() => {
-            // Alert.alert("Modal has been closed.");
-            // this.setModalVisible(!modalVisible);
-          }}
         >
           <View style={{ zIndex: 1000, flex: 1, backgroundColor: 'black', opacity: 0.5 }}>
           </View>
           <View style={{ position: 'absolute', opacity: 1.0, zIndex: 2000 }}>
-              <RedSquare isClone={true} />
+              <RedSquare 
+                isClone={true} 
+                originalElementX={this.state.originalElementX} 
+                originalElementY={this.state.originalElementY} 
+              />
             </View>
         </Modal>
       </SafeAreaView>
